9ee4787ed1a9e5a0aa829a6050efb4ca0047af37
Glen Aries C. Viernes <glenaries03@gmail.com>
2013-10-08 18:04:30 +0800

    master:test in eclipse

--- src/com/example/edetailing/SplashActivity.java
+++ src/com/example/edetailing/SplashActivity.java
@@ -9,6 +9,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //
         setContentView(R.layout.activity_splash);
     }
 
